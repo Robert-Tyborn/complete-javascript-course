a73e24cece623be8513153eef9f2635dc77bd3de
Robert-Tyborn <144322797+Robert-Tyborn@users.noreply.github.com>
2023-11-04 14:07:28 +0100

    ..

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,199 @@
+// let js = 'amazing';
+// console.log(4 + 4 + 19 + 56 -10);
+
+// let firstName = "Matilda"
+
+// // bla bla bla
+// console.log(firstName + " is " + js)
+// console.log(firstName + " is " + js)
+// console.log(firstName + " is " + js)
+
+// let threeYears = 3; 
+
+// console.log(firstName + threeYears + js)
+// console.log(country, continent, population)
+
+// true;
+
+// console.log(true)
+
+// const country = 'Sweden'
+// const continent = 'Europe'
+// const population = '10 million'
+
+// let javaScruptIsFun = true;
+// // console.log(typeof true)
+// console.log(typeof javaScruptIsFun)
+// // console.log(typeof 23)
+// // console.log(typeof "robert")
+
+// javaScruptIsFun = "YES!"
+
+// console.log(typeof javaScruptIsFun)
+
+// let year; 
+// console.log(typeof year)
+
+// year = 1923;
+// console.log(typeof year)
+
+// const isIsland = false;
+// const language = "Swedish";
+
+// isIsland = true
+
+//  console.log(typeof isIsland, population, continent, country, language)
+
+
+// const birthYear = 1991;
+
+// let myAge = 45; 
+
+// myAge = 46; 
+// console.log(myAge)
+// const now = 2038;
+// const ageRobert = now - 1991;
+// const ageSarag = now - 2018
+// console.log(ageRobert, ageSarag)
+
+// console.log(ageRobert * 2, ageSarag / 10, 2 ** 9)
+// // 2 ** 3 means 2 to the power of 3. Which = 2 * 2 * 2
+
+// const firstName = "R0bert"
+// const lastName = "Tyborn"
+
+// console.log(firstName + " " + lastName)
+
+// let x = 2 + 5
+// x += 10
+// x += 12
+// x /= 10
+// x++
+// x--
+// let y = x * x 
+
+// console.log(x, y)
+
+// console.log(ageRobert > ageSarag)
+// console.log(ageSarag >= 18)
+
+// const isFullAge = (ageSarag >= 18)
+// console.log(now - 1991 > now - 2018)
+
+// const now = 2038;
+// const ageRobert = now - 1991;
+// const ageSarag = now - 2018
+// console.log(ageRobert, ageSarag)
+
+// console.log(now - 1991 > now - 2018)
+
+// const averageAge = (ageRobert + ageSarag) / 2;
+// console.log(ageRobert, ageSarag)
+
+// const massMark = 95;
+// const heightMark = 1.98;
+// const massJohn = 92;
+// const heightJohn = 1.78;
+
+// const BMIMark =  massMark / (heightMark * heightMark);
+// const BMIJohan = massJohn / heightJohn ** 2;
+// const markHigherBMI = BMIMark > BMIJohan
+// console.log(BMIMark, BMIJohan, markHigherBMI)
+// console.log(BMIMark > BMIJohan)
+
+// const firstName = "Robert";
+// const job = "QA tester";
+// const birthYear = 1978;
+// const age = 2023 - birthYear
+
+// const robert = "I'm " + firstName +  " and I works as a " + job + " and I'm " + (birthYear - 2023)+ " years old"
+// const roybert = `I'm ${firstName}, a ${age} year old ${job}!`
+// console.log(robert)
+// console.log(roybert)
+
+// console.log(`string with
+// multiple
+// lines`)
+
+// const age = 15; 
+// // const isOldEnough = age >= 18;
+
+// if(age >= 18){
+//     console.log("Old enough")
+// } else {
+//     const yearsLeft = 18 - age;
+//     console.log(`Not old enough, you can 
+//     start in ${yearsLeft} years`)
+// }
+
+// const birthYear = 2991;
+// let century;
+
+// if(birthYear <= 2000){
+//     century = "20th";
+// } else {
+//     century = "21st"
+// }
+// console.log(century)
+
+
+// const massMark = 18;
+// const heightMark = 1.69;
+// const massJohn = 92;
+// const heightJohn = 1.95;
+
+// const BMIMark = massMark / (heightMark * heightMark);
+// const BMIJohn = massJohn / (heightJohn * heightJohn);
+// console.log(BMIMark, BMIJohn);
+
+// /* Write your code below. Good luck! 🙂 */
+// if (BMIMark < BMIJohn) {
+//     console.log(`Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})!`);
+// } else {
+//     console.log(`John's BMI (${BMIJohn}) is higher than Mark's (${BMIMark})!`);
+// }
+
+// const input = "1992";
+// const summa = input + input
+
+// console.log(input)
+// console.log(Number(input))
+// console.log(summa)
+// console.log(Number(input) + Number(input))
+
+// const output = 1992;
+
+// console.log(String(output))
+
+// console.log(Boolean(0));
+// console.log(Boolean(undefined));
+// console.log(Boolean("jonas"));
+// console.log(Boolean({}));
+// console.log(Boolean(''));
+
+// // falsy values: 0, '', undefined, null, NaN
+
+// let money
+// if(money){
+//     console.log("Spend")
+// } else {
+//     console.log("save") // blir denna för att money är falsy
+// }
+
+
+// const age = 18;
+// if (age === 18) console.log("adult")
+// if (age >= 18){
+//     console.log("adult")
+// } else if (age < 18) {
+//     console.log("not adult")
+// }
+
+let favorite = prompt("Enter your favorite number!");
+console.log(Number(favorite));
+
+console.log(typeof favorite);
+
+if (favorite == 23) {
+    console.log("cool 23 is amaxing")
+}
